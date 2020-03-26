0e1e0af2260c67c72e8e477cdf948eb77b470303
AvramPop <dany4god@gmail.com>
2020-03-26 16:47:11 +0200

    added prof's stub

--- docs/api/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"domain"},{"l":"domain.exceptions"},{"l":"domain.validators"},{"l":"repository"},{"l":"service"},{"l":"UI"}]
