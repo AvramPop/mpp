ed3a849a074f4ab7fb4c58741ca1be3ffcdcff55
AvramPop <dany4god@gmail.com>
2020-04-02 14:21:46 +0300

    db credentials read from file

--- remoting/server/src/main/java/ro/ubb/remoting/server/config/JDBCConfig.java
+++ remoting/server/src/main/java/ro/ubb/remoting/server/config/JDBCConfig.java
@@ -2,8 +2,10 @@ package ro.ubb.remoting.server.config;
 
 
 import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.core.JdbcOperations;
 import org.springframework.jdbc.core.JdbcTemplate;
 
@@ -11,7 +13,16 @@ import javax.sql.DataSource;
 
 
 @Configuration
+@PropertySource("classpath:app.properties")
 public class JDBCConfig {
+
+  @Value( "${url}" )
+  private String url;
+  @Value( "${username}" )
+  private String username;
+  @Value( "${password}" )
+  private String password;
+
   @Bean
   JdbcOperations jdbcOperations() {
     JdbcTemplate jdbcTemplate = new JdbcTemplate();
@@ -26,9 +37,9 @@ public class JDBCConfig {
     BasicDataSource basicDataSource = new BasicDataSource();
 
     //TODO use env props (or property files)
-    basicDataSource.setUrl("jdbc:postgresql://localhost:5432/ProblemAssignments");
-    basicDataSource.setUsername("postgres");
-    basicDataSource.setPassword("admin");
+    basicDataSource.setUrl(url);
+    basicDataSource.setUsername(username);
+    basicDataSource.setPassword(password);
     basicDataSource.setInitialSize(2);
 
     return basicDataSource;
